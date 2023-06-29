912a2f7d6514ae13a726404e006be212836ba891
harithummala <135719702+harithummala@users.noreply.github.com>
2023-06-29 13:09:05 -0400

    haripriya.js

--- DockerHub/src/helloworld.js
+++ DockerHub/src/helloworld.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/helloworld*', (req, res) => {
-  res.send('Hi World!')
+  res.send('Hello, This is Haripriya!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`haripriya app listening on port ${port}`)
 })
