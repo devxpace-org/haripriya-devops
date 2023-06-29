4a775ce5a5425959f830352f714a507f553c8e2a
harithummala <135719702+harithummala@users.noreply.github.com>
2023-06-29 13:20:20 -0400

    Create praneeth.js

---
+++ DockerHub/src/praneeth.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/helloworld*', (req, res) => {
+  res.send('Hello, This is Praneeth!')
+})
+
+app.listen(port, () => {
+  console.log(`praneeth app listening on port ${port}`)
+})
